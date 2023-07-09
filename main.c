6aaeee64afea777d971d22900f086269ff36aca1
kornelu123 <kornelu12@gmail.com>
2023-07-09 03:20:26 +0200

    working window

--- main.c
+++ main.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 #include <xcb/xcb.h>
 #include <inttypes.h>
 
 int main(){
-	int i, screenNum;
-	xcb_connection_t *conn = xcb_connect(NULL, &screenNum);
-	
-	const xcb_setup_t *setup = xcb_get_setup(conn);
-	xcb_screen_iterator_t iter = xcb_setup_roots_iterator(setup);
-	
-	for(i = 0; i < screenNum; i++){
-		xcb_screen_next(&iter);
-	}
-	
-	xcb_screen_t *screen = iter.data;
+	xcb_connection_t *con;
+	xcb_screen_t	*screen;
+	xcb_window_t	win;
 
-	printf("cwel");
-	printf("Informations of screen %d:\n", screen->root);
+	con = xcb_connect(NULL,NULL);
+	screen = xcb_setup_roots_iterator(xcb_get_setup(con)).data;
 
-	xcb_disconnect(conn);
+	win = xcb_generate_id(con);
+	
+	xcb_create_window(con,		/*connection*/
+		XCB_COPY_FROM_PARENT,	/*depth (same as root)*/
+		win,			/*window id*/
+		screen->root,		/*parent window*/
+		0, 0,			/* x , y*/
+		150,150,		/*width, height*/
+		10,			/*border width*/
+		XCB_WINDOW_CLASS_INPUT_OUTPUT, /*class*/
+		screen->root_visual,	/*visual*/
+		0,NULL);		/*masks*/
+	
+	xcb_map_window(con,win);
+	
+	xcb_flush(con);
+
+	pause();
 
 	return 0;
 }
