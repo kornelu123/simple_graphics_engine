1486ddf9f3ecb2d7f5e8bec34571a9163eaea752
kornelu123 <kornelu12@gmail.com>
2023-07-09 23:51:29 +0200

    floating square in floating window

--- main.c
+++ main.c
@@ -2,18 +2,63 @@
 #include <stdlib.h>
 #include <xcb/xcb.h>
 #include <unistd.h>
+#include <string.h>
+#include <xcb/xproto.h>
+
+void set_floating_atoms(xcb_connection_t *con, xcb_drawable_t win ){
+	xcb_intern_atom_cookie_t cookie, cookie1;
+	xcb_intern_atom_reply_t *reply, *reply1;
+	
+	cookie = xcb_intern_atom(con, 1, strlen("_NET_WM_WINDOW_TYPE"), "_NET_WM_WINDOW_TYPE");
+	reply = xcb_intern_atom_reply(con, cookie, 0);
+	
+	cookie1 = xcb_intern_atom(con, 0, strlen("_NET_WM_WINDOW_TYPE_SPLASH"),"_NET_WM_WINDOW_TYPE_SPLASH");
+	reply1 = xcb_intern_atom_reply(con, cookie1, 0);
+
+	xcb_change_property(con, XCB_PROP_MODE_REPLACE, win, reply->atom, 4 , 32 , 1, &reply1->atom);
+}
+
+void go_right(xcb_point_t polyline[]){
+	polyline[0].x++;
+}
+
+void go_left(xcb_point_t polyline[]){
+	polyline[0].x--;
+}
+
+int handle_move(xcb_point_t polyline[],int count){
+	if(count < 150){
+		polyline[0].x = count;
+	}
+	if(count > 150){
+		polyline[0].x = 300-count;	
+	}
+	if(count == 300){
+		count = 0;	
+	}
+	count++;
+	return count;
+}
+
+void clean_win(xcb_connection_t *con, xcb_drawable_t win){
+	xcb_clear_area(con, 1, win, 0, 0, 200, 200);
+	xcb_flush(con);
+}
 
 int main(){
 	xcb_connection_t    *con;
 	xcb_screen_t	      *screen;
 	xcb_drawable_t      win;
-  xcb_gcontext_t      b_foreground;
-  xcb_gcontext_t      w_foreground;
+  xcb_gcontext_t      b_foreground; 
   uint32_t            mask = 0;
   uint32_t            values[2];
 
- 
-  xcb_point_t polyline[1];
+  xcb_point_t polyline[] = {
+{0,75},
+{50,0},
+{0,50},
+{-50,0}, 
+{0,-50}};
 
   //opening connection to X server
 	con = xcb_connect(NULL,NULL);
@@ -29,16 +74,6 @@ int main(){
   values[0] = screen->black_pixel;
   values[1] = 0;
   xcb_create_gc(con, b_foreground, win, mask, values);
-	
-
-  //create white foreground graphic context
-  w_foreground = xcb_generate_id(con);
-  mask = XCB_GC_FOREGROUND | XCB_GC_GRAPHICS_EXPOSURES;
-  values[0] = screen->white_pixel;
-  values[1] = 0;
-  xcb_create_gc(con, w_foreground, win, mask, values);
-
-
 
   //Ask for window's id 
   win = xcb_generate_id(con);
@@ -52,23 +87,33 @@ int main(){
                     win , 
                     screen->root , 
                     0, 0, 
-                    1920,1200, 
+		    200,200, 
                     10, 
                     XCB_WINDOW_CLASS_INPUT_OUTPUT, 
                     screen->root_visual, 
                     mask , values);
   
   //Map window on the screen 
+  set_floating_atoms(con,win);
   xcb_map_window(con, win);
-  for(int i=0;i<1200;i++){
-    polyline[0].y  = i;
-    for(int j=0;j<1920;j+=2){
-      polyline[0].x = j;
-      xcb_poly_point(con,XCB_COORD_MODE_ORIGIN,win,b_foreground,1,polyline);
-      xcb_flush(con);
-    }
-  }
-  //Flush the request
   xcb_flush(con);
-  return 0;
+  xcb_generic_event_t *e;
+  int count = 0;
+  while((e = xcb_wait_for_event(con))){
+    switch (e->response_type & ~0x80){
+      case XCB_EXPOSE:
+	xcb_poly_line(con, XCB_COORD_MODE_PREVIOUS,win ,b_foreground, 5, polyline);
+	xcb_flush(con);
+	count = handle_move(polyline,count);
+	usleep(10000);
+	clean_win(con,win);
+   	break;
+      default:
+	break;
+  } 
+  free(e);
+}
+  xcb_flush(con); 
+  sleep(3);
+return 0;
 }
